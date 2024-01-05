b7464e48b0a06d2fdf7e50c8051b49a6a5f0a5a7
Vincent Mow <vincent.mow@csiro.au>
2024-01-05 22:30:00 +1100

    Fixed print dimensions.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1758,12 +1758,12 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -5
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
